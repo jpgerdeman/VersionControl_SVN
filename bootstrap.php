cd657f0293d6eab6da293307765ad7b41c7ebac0
jpgerdeman <johnpatrick@gerdeman.de>
2013-11-26 09:31:46 +0100

    Corrected bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -53,8 +53,8 @@
  */
 require_once 'System.php';
 require_once 'PEAR/Exception.php';
-require_once __DIR__.DIRECTORY_SEPARATOR.'SVN.php';
-require_once __DIR__.DIRECTORY_SEPARATOR.'SVN'.DIRECTORY_SEPARATOR.'Exception.php';
-require_once __DIR__.DIRECTORY_SEPARATOR.'SVN'.DIRECTORY_SEPARATOR.'Exception.php';
-require_once __DIR__.DIRECTORY_SEPARATOR.'SVN'.DIRECTORY_SEPARATOR.'Command.php';
-require_once __DIR__.DIRECTORY_SEPARATOR.'SVN'.DIRECTORY_SEPARATOR.'Parser/XML.php';
+require_once __DIR__.DIRECTORY_SEPARATOR.'VersionControl'.DIRECTORY_SEPARATOR.'SVN.php';
+require_once __DIR__.DIRECTORY_SEPARATOR.'VersionControl'.DIRECTORY_SEPARATOR.'SVN'.DIRECTORY_SEPARATOR.'Exception.php';
+require_once __DIR__.DIRECTORY_SEPARATOR.'VersionControl'.DIRECTORY_SEPARATOR.'SVN'.DIRECTORY_SEPARATOR.'Exception.php';
+require_once __DIR__.DIRECTORY_SEPARATOR.'VersionControl'.DIRECTORY_SEPARATOR.'SVN'.DIRECTORY_SEPARATOR.'Command.php';
+require_once __DIR__.DIRECTORY_SEPARATOR.'VersionControl'.DIRECTORY_SEPARATOR.'SVN'.DIRECTORY_SEPARATOR.'Parser/XML.php';
